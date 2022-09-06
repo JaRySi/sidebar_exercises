2d065325b35c4cee6019b31437a8bbfa0b010185
Jacob Sieffert <jacob.r.sieffert@gmail.com>
2022-09-06 16:19:17 -0700

    9/06/22 session.

---
+++ ruby_basics/variable_scope/4.rb
@@ -0,0 +1,11 @@
+a = 'Xyzzy'
+
+def my_value(b)
+  b[2] = '-'
+end
+
+ my_value(a)
+ puts a
+
+# Predict: 'Xyzzy'
+# Result: 'Xy-zy'
---
+++ ruby_basics/variable_scope/6.rb
@@ -0,0 +1,11 @@
+a = 7
+
+def my_value(b)
+  b = a + a
+end
+
+my_value(a)
+puts a
+
+# Predict: 7
+# Result: 6.rb:4:in `my_value': undefined local variable or method `a' for main:Object (NameError)
